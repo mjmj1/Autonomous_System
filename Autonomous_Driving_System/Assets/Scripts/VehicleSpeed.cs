60b02f47728b73dc0c450e9952ec719d158a4475
M1A1Z <okjanghee@gmail.com>
2023-02-02 20:23:15 +0900

    TrafficScreen(Last)

--- Autonomous_Driving_System/Assets/Scripts/VehicleSpeed.cs
+++ Autonomous_Driving_System/Assets/Scripts/VehicleSpeed.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.Threading;
 
 public class VehicleSpeed : MonoBehaviour
 {
@@ -14,6 +15,8 @@ public class VehicleSpeed : MonoBehaviour
     [SerializeField]
     public TextMeshProUGUI meterPerSecond, kilometersPerHour;
 
+    float Distance = -52.34f;
+
     float GetSpeed()
     {
         float speed = (((transform.position - lastPosition).magnitude) / Time.deltaTime);
@@ -22,11 +25,11 @@ public class VehicleSpeed : MonoBehaviour
         return speed;
     }
 
-
     void FixedUpdate()
     {
         Speed = GetSpeed();
-        meterPerSecond.text = string.Format("{0:00.00} m/s", Speed);
+        Distance += Speed * Time.deltaTime;
+        meterPerSecond.text = string.Format("{0:00.00} m", Distance);
         kilometersPerHour.text = string.Format("{0:00.00} km/h", Speed * 3.6f);
         Chim.transform.rotation = Quaternion.Euler(0, 0, 22 - (Speed * 3.6f));
     }
